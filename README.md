413839064be5e48e3b32176dfe0af03796c9bd26
baba-s <shotababa1206@gmail.com>
2022-11-09 19:17:25 +0900

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# UniJsonFormatter
+# Kogane Json Formatter
 
 Json 形式の文字列を読みやすい形式に整形する機能
 
@@ -29,5 +29,5 @@ public class Example : MonoBehaviour
 
 ## 謝辞
 
-* このリポジトリは下記のサイト様で公開されているソースコードを使用させていただいております  
+* このリポジトリは下記のサイト様で公開されているソースコードを使用させていただいております
     * https://qiita.com/sh_akira/items/9829de2d76f8f36361aa
---
+++ Runtime/JsonFormatter.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace Kogane
+{
+    public static class JsonFormatter
+    {
+        public static string ToReadable( string json )
+        {
+            if ( string.IsNullOrEmpty( json ) ) return json;
+
+            var i          = 0;
+            var indent     = 0;
+            var quoteCount = 0;
+            var position   = -1;
+            var sb         = new StringBuilder();
+            var lastIndex  = 0;
+
+            while ( true )
+            {
+                if ( i > 0 && json[ i ] == '"' && json[ i - 1 ] != '\\' ) quoteCount++;
+
+                if ( quoteCount % 2 == 0 )
+                {
+                    if ( json[ i ] == '{' || json[ i ] == '[' )
+                    {
+                        indent++;
+                        position = 1;
+                    }
+                    else if ( json[ i ] == '}' || json[ i ] == ']' )
+                    {
+                        indent--;
+                        position = 0;
+                    }
+                    else if ( json.Length > i && json[ i ] == ',' && json[ i + 1 ] == '"' )
+                    {
+                        position = 1;
+                    }
+
+                    if ( position >= 0 )
+                    {
+                        sb.AppendLine( json.Substring( lastIndex, i + position - lastIndex ) );
+                        sb.Append( new string( ' ', indent * 4 ) );
+                        lastIndex = i + position;
+                        position  = -1;
+                    }
+                }
+
+                i++;
+
+                if ( json.Length <= i )
+                {
+                    sb.Append( json.Substring( lastIndex ) );
+                    break;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
